02e3bf7dd2d512e36119b4b3f107086d90b708ad
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-06-16 13:43:49 +0530

    fix eslint errors

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
+export default (app) => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log('Yay, the app was loaded!');
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.on('issues.opened', async (context) => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
+    return context.github.issues.createComment(issueComment);
+  });
 
-  app.on('issue_comment.created', context => {
+  app.on('issue_comment.created', (context) => {
     if (context.payload.comment.body.match(/\/remind\s*me/)) {
-      context.log('This issue comment is a reminder request.')
-      const issueComment = context.issue({ body: 'I will remind you of this issue' })
-      return context.github.issues.createComment(issueComment)
-    } else {
-      context.log('Sadly, this issue comment is a not reminder request.')
+      context.log('This issue comment is a reminder request.');
+      const issueComment = context.issue({ body: 'I will remind you of this issue' });
+      return context.github.issues.createComment(issueComment);
     }
-  })
+    context.log('Sadly, this issue comment is a not reminder request.');
+    return undefined;
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
